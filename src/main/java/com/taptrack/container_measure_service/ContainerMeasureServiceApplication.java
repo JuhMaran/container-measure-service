210ee03b097779ebc4aaae838b13ff4822473397
JuhMaran <julianemaran@gmail.com>
2025-10-18 11:19:52 -0300

    add new project

---
+++ src/main/java/com/taptrack/container_measure_service/ContainerMeasureServiceApplication.java
@@ -0,0 +1,13 @@
+package com.taptrack.container_measure_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerMeasureServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerMeasureServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taptrack/container_measure_service/ContainerMeasureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taptrack.container_measure_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerMeasureServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
